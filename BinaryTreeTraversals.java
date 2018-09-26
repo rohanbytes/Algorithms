1fa957d0abc26e261ae35b2371d86d0ed3a8aff1
Rohan Saxena <rohansaxenabits@gmail.com>
2018-09-26 06:26:13 +0530

    Added BinaryTreeTraversals

---
+++ BinaryTreeTraversals.java
@@ -0,0 +1,57 @@
+class Node<T>{
+    public T data;
+    public Node<T> leftNode, rightNode;
+
+    public Node(T data){
+        this.data = data;
+    }
+}
+
+public class BinaryTreeTraversals {
+
+    public static void preOrderTraversal(Node<Integer> root){
+        if(root == null){
+            return;
+        }
+        System.out.print(root.data + " ");
+        preOrderTraversal(root.leftNode);
+        preOrderTraversal(root.rightNode);
+    }
+
+    public static void inOrderTraversal(Node<Integer> root){
+        if(root == null){
+            return;
+        }
+        inOrderTraversal(root.leftNode);
+        System.out.print(root.data + " ");
+        inOrderTraversal(root.rightNode);
+    }
+
+    public static void postOrderTraversal(Node<Integer> root){
+        if(root == null){
+            return;
+        }
+        postOrderTraversal(root.leftNode);
+        postOrderTraversal(root.rightNode);
+        System.out.print(root.data + " ");
+    }
+
+    public static void main(String[] args){
+        Node<Integer> root = new Node<>(10);
+        Node<Integer> a = new Node<>(20);
+        Node<Integer> b = new Node<>(30);
+        Node<Integer> c = new Node<>(40);
+        Node<Integer> d = new Node<>(50);
+
+        root.leftNode = a;
+        root.rightNode = b;
+        b.leftNode = c;
+        b.rightNode = d;
+
+        preOrderTraversal(root);
+        System.out.println();
+        inOrderTraversal(root);
+        System.out.println();
+        postOrderTraversal(root);
+    }
+}
